bcceaab5f9f7c5f347f5aefc46a6a516320e35a9
FakeGods <w.leszkowicz99@gmail.com>
2024-06-12 23:49:06 +0200

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,39 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
+import logo from "../assets/logo.svg";
+
+const Nav = () => {
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center h-10 text-white text-decoration-none bg-transparent pr-5">
+      <div className="flex justify-between items-center">
+        <img
+          src={logo}
+          alt="portfolio_logo"
+          className="w-20 object-contain pl-7 text-xl"
+        />
+      </div>
+      <div className="flex justify-between items-center">
+        <div className="relative flex items-center pl-4">
+          <a href="#hero" className="text-black text-decoration-none">
+            Home
+          </a>
+        </div>
+        <div className="relative flex items-center pl-4">
+          <a href="#about" className="text-black text-decoration-none">
+            About
+          </a>
+        </div>
+        <div className="relative flex items-center pl-4">
+          <a href="#projects" className="text-black text-decoration-none">
+            Projects
+          </a>
+        </div>
+        <div className="relative flex items-center pl-4">
+          <a href="#contact" className="text-black text-decoration-none">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
-export default Navbar;
+export default Nav;
