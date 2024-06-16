8948e2554f14db9db40d7aa1f439275e14fb1583
FakeGods <w.leszkowicz99@gmail.com>
2024-06-16 21:49:42 +0200

    create

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
 const Footer = () => {
   return (
     <div className="">
-      <div className="footer">
-        <h3>Copyright © 2024. All rights are reserved</h3>
-      </div>
+      <h3>Copyright © 2024. All rights are reserved</h3>
     </div>
   );
 };
