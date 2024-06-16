1882bcfbdbb92f56ef1e5de27c32d1e601c17271
FakeGods <w.leszkowicz99@gmail.com>
2024-06-16 21:53:47 +0200

    adding footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { GridBackgroundDemo } from "./components/Background";
 import Nav from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="bg-bkg text-content">
       <Nav />
       <GridBackgroundDemo />
+      <Footer />
     </div>
   );
 }
