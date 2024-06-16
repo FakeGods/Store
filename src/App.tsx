b53f27dc68274aa6119fc11496f892f4b81681e6
FakeGods <w.leszkowicz99@gmail.com>
2024-06-16 21:48:25 +0200

    dark

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Nav from "./components/Navbar";
 
 export default function App() {
   return (
-    <>
+    <div className="bg-bkg text-content">
       <Nav />
       <GridBackgroundDemo />
-    </>
+    </div>
   );
 }
