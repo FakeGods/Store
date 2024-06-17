ad170ab5c660457ed59ed8f98303be6f9b2813f3
FakeGods <w.leszkowicz99@gmail.com>
2024-06-17 20:16:24 +0200

    yearNow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+const yearNow = new Date().getFullYear();
+
 export default function App() {
   return (
     <footer className="bg-neutral-200 text-center text-white dark:bg-neutral-600">
@@ -72,13 +74,7 @@ export default function App() {
 
       {/* <!--Copyright section--> */}
       <div className="bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
-        © 2023 Copyright:
-        <a
-          className="text-neutral-800 dark:text-neutral-400"
-          href="https://tw-elements.com/"
-        >
-          TW Elements
-        </a>
+        © {yearNow} Copyright
       </div>
     </footer>
   );
