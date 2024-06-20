1e834eef23152ee0720c3f1101e4d2abbc0f869c
FakeGods <w.leszkowicz99@gmail.com>
2024-06-20 16:11:52 +0200

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { GridBackgroundDemo } from "./components/Background";
 import Nav from "./components/Navbar";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <div className="bg-bkg text-content">
       <Nav />
+      <Header />
+      <Hero />
       <GridBackgroundDemo />
       <Footer />
     </div>
