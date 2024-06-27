e6790fd699824e6f9d5ab0179e44aa9435e082bd
FakeGods <w.leszkowicz99@gmail.com>
2024-06-27 23:21:13 +0200

    no hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { GridBackgroundDemo } from "./components/Background";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
 
 export default function App() {
   return (
